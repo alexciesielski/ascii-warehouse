a59c0898c59d0dc2bc7a068859d8025b4802b24d
Alexander Ciesielski <alexander@ciesielski.co>
2016-06-09 17:49:59 +0200

    Implemented Total price in Shopping cart

--- src/app/shared/components/shopping-cart/shopping-cart.component.ts
+++ src/app/shared/components/shopping-cart/shopping-cart.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, DoCheck } from '@angular/core';
 import { Product } from '../../product.model';
+import { FormatCurrency } from '../../format-currency.pipe';
 
 @Component({
   moduleId: module.id,
   selector: 'app-shopping-cart',
   templateUrl: 'shopping-cart.component.html',
-  styleUrls: ['shopping-cart.component.css']
+  styleUrls: ['shopping-cart.component.css'],
+  pipes: [FormatCurrency]
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent implements OnInit, DoCheck {
   @Input('products') shoppingCart: Product[] = [];
+  previousShoppingCartCount: number = 0;
+  total: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngDoCheck() {
+    if (this.shoppingCart.length > this.previousShoppingCartCount) {
+      this.previousShoppingCartCount = this.shoppingCart.length;
+      this.calculateTotal();
+    }
+  }
+
+  calculateTotal() {
+    let total = 0;
+    for (let i = 0; i < this.shoppingCart.length; i++) {
+      total += this.shoppingCart[i].price;
+    }
+    this.total = total;
   }
 
   removeProduct(product) {
