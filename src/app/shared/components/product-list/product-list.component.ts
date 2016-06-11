a840fac8a1d174bb3928d394752185eb7f5402b8
Alexander Ciesielski <alexander@ciesielski.co>
2016-06-11 17:58:11 +0200

    Preload products and display only if scrolled to bottom

--- src/app/shared/components/product-list/product-list.component.ts
+++ src/app/shared/components/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Product, ProductService, ProductComponent, AdComponent } from '../../index';
+import { Ad, Product, ProductService, ProductComponent, AdComponent } from '../../index';
 
 import { ScrollSpy } from '../../scroll-spy.directive'; // import separately to avoid error "Unexpected directive value 'undefined'" 
 import { FormatTimestamp } from '../../format-timestamp.pipe'; // same here
@@ -14,39 +14,91 @@ import { FormatCurrency } from '../../format-currency.pipe'; // same here
   pipes: [FormatTimestamp, FormatCurrency]
 })
 export class ProductListComponent {
-  @Input('items') items: any[];
-
   @Output('addedToCart') addedToCart: EventEmitter<Product> = new EventEmitter<Product>();
-  @Output('preload') preload: EventEmitter<any> = new EventEmitter<any>();
+  @Output('scrolledNearBottom') scrolledNearBottom: EventEmitter<any> = new EventEmitter<any>();
 
-  scrolledToBottom: boolean = false;
-  preloadAtScrollPercentage: number = 90;
+  // list of products and ads (at every 20th position)
+  private items: any[] = [];
+  private productsCount: number = 0;
+
+  // list of products that are about to be displayed, when user scrolls to bottom
+  private preloadedProducts: Product[] = [];
+
+
+  private isScrolledNearBottom: boolean = false;
+  private scrolledNearBottomPercentage: number = 80;
+  private scrolledToBottomPercentage: number = 99;
 
   constructor() { }
 
-  addToCart(product) {
+  public addProducts(products: Product[], shouldShowProducts?: boolean) {
+    this.preloadedProducts = Array.prototype.concat(this.preloadedProducts, products);
+    this.insertAdsIntoItems();
+
+    if (shouldShowProducts) {
+      this.showProducts();
+    }
+  }
+
+  public getProductsCount() {
+    return this.productsCount;
+  }
+
+  public resetItems() {
+    this.items = [];
+    this.productsCount = 0;
+  }
+
+  private showProducts() {
+    this.productsCount += this.preloadedProducts.length;;
+    this.items = Array.prototype.concat(this.items, this.preloadedProducts);
+    this.preloadedProducts = [];
+  }
+
+  private insertAdsIntoItems() {
+    let multiplesOfTwenty = Math.floor(this.productsCount / 20);
+
+    for (let i = 0; i < multiplesOfTwenty; i++) {
+      let index = (i + 1) * 20;
+      if (!this.isAd(this.items[index])) {
+        this.items.splice(index, 0, new Ad());
+        console.log('inserted ad at ' + index);
+      }
+    }
+  }
+
+  private addToCart(product) {
     //console.log('Added to cart: ' + product.face);
     this.addedToCart.emit(product);
   }
 
-  onScroll(percentageScrolled) {
+  private onScroll(percentageScrolled) {
     //console.log('Scrollposition: ' + percentageScrolled + '%');
 
-    if (percentageScrolled < this.preloadAtScrollPercentage) {
-      this.scrolledToBottom = false;
+    if (percentageScrolled < this.scrolledNearBottomPercentage) {
+      this.isScrolledNearBottom = false;
     }
 
-    if (this.scrolledToBottom === false && percentageScrolled > this.preloadAtScrollPercentage) {
-      this.scrolledToBottom = true;
-      this.preload.emit(true);
+    // emit event only once (if not yet scrolled near bottom)
+    if (this.isScrolledNearBottom === false && percentageScrolled > this.scrolledNearBottomPercentage) {
+      this.isScrolledNearBottom = true;
+      this.scrolledNearBottom.emit(true);
+    }
+
+    if (this.isScrolledNearBottom && percentageScrolled >= this.scrolledToBottomPercentage) {
+      //this.scrolledToBottom.emit(true);
+      // Scrolled to bottom
+      this.showProducts();
     }
   }
 
-  isProduct(item: any) {
-    return !!item.face;
+  private isProduct(item: any) {
+    return (item && item.hasOwnProperty('face'));
   }
 
-  isAd(item: any) {
-    return !!item.url;
+  private isAd(item: Object) {
+    // fine for demo purposes, should be done differently in larger app  
+    return (item && item.hasOwnProperty('url'));
   }
+
 }
--- src/app/xteam.component.ts
+++ src/app/xteam.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { Ad, Product, AdComponent, ProductListComponent, ShoppingCartComponent, ProductService } from './shared/index';
 import { Observable } from 'rxjs/Observable';
 
@@ -10,18 +10,17 @@ import { Observable } from 'rxjs/Observable';
   providers: [ProductService],
   directives: [ProductListComponent, AdComponent, ShoppingCartComponent]
 })
-export class XteamAppComponent {
+export class XteamAppComponent implements OnInit {
+  @ViewChild('productList') productList: ProductListComponent;
   @ViewChild('shoppingCart') shoppingCart: ShoppingCartComponent;
 
   title = 'Discount Ascii Warehouse';
-  error: any;
+  error: any = null;
 
   isLoading = false;
   isEndOfCatalogue = false;
 
-  // list of products and ads (at every 20th position)
-  items: any[] = [];
-  productsCount: number = 0;
+  preloadedProducts: Product[] = [];
 
   sortOptions: string[] = [];
   selectedSortOption: string;
@@ -31,16 +30,14 @@ export class XteamAppComponent {
   ) { }
 
   ngOnInit() {
-    // Initialize form
     this.sortOptions = ProductService.SORT_OPTIONS;
     this.selectedSortOption = this.sortOptions[0];
 
-    this.getProducts(20);
+    this.preloadProducts(true, 20);
   }
 
-  resetItems() {
-    this.items = [];
-    this.productsCount = 0;
+  resetList() {
+    this.productList.resetItems();
   }
 
   toggleShoppingCartVisibility() {
@@ -54,70 +51,56 @@ export class XteamAppComponent {
 
   applySort() {
     console.log(this.selectedSortOption);
-    this.resetItems();
-    this.getProducts(20);
+    this.resetList();
+    this.preloadProducts(true, 20);
   }
 
-  insertAdsIntoItems() {
-    let multiplesOfTwenty = Math.floor(this.productsCount / 20);
-
-    for (let i = 0; i < multiplesOfTwenty; i++) {
-      let index = (i + 1) * 20;
-      if (!this.isAd(this.items[index])) {
-        this.items.splice(index, 0, this.createAd());
-        console.log('inserted ad at ' + index);
-      }
-    }
+  showPreloadedProducts() {
+    console.log('show preloaded products');
+    this.productList.addProducts(this.preloadedProducts);
+    this.preloadedProducts = [];
   }
 
-  addProductsToItems(products: Product[]) {
-    this.productsCount += products.length;
-    this.items = Array.prototype.concat(this.items, products);
-  }
 
-  createAd(): Ad {
-    // should be moved to own service in production app
-    return new Ad();
-  }
-
-  loadMoreProducts() {
-    this.getProducts(50);
-  }
-
-  isAd(item: Object) {
-    // fine for demo purposes, should be done differently in larger app  
-    return (item && item.hasOwnProperty('url'));
-  }
-
-  getProducts(productsToFetch: number) {
+  // todo:
+  // refactor displaying preloaded products into product list
+  // this component only has to know when to preload products
+  // not when to show them
+  preloadProducts(shouldShowProducts: boolean, productsToFetch?: number) {
+    console.log('preloadProducts(showProducts: ', + !!shouldShowProducts + ', limit: ' + productsToFetch);
     if (!this.isEndOfCatalogue) {
-      let sort = this.selectedSortOption;
-      let limit = productsToFetch || 50;
-      let skip = this.productsCount;
 
-      this.isLoading = true;
+      if (this.preloadedProducts.length > 0) {
+        console.log(this.preloadedProducts.length);
+        this.showPreloadedProducts();
+      } else {
+        let sort = this.selectedSortOption;
+        let limit = productsToFetch || 50;
+        let skip = 0;
+        if (this.productList) skip = this.productList.getProductsCount(); // productList at OnInit not yet initialized
 
-      this._productsService.getProducts(sort, limit, skip).subscribe(
-        products => {
-          let productsFetchedCount = products.length;
+        this.isLoading = true;
+        this._productsService.getProducts(sort, limit, skip).subscribe(
+          products => {
+            let productsFetchedCount = products.length;
 
-          if (productsFetchedCount > 0) {
-            this.addProductsToItems(products);
-            this.insertAdsIntoItems();
-            console.log('current products count:', this.productsCount);
-          }
+            if (productsFetchedCount > 0) {
+              this.productList.addProducts(products, shouldShowProducts);
+              console.log('current products count:', this.productList.getProductsCount());
+            }
 
-          if (productsFetchedCount < limit) {
-            console.log('end of catalogue');
-            this.isEndOfCatalogue = true;
-          }
+            if (productsFetchedCount < limit) {
+              this.isEndOfCatalogue = true;
+              console.log('end of catalogue');
+            }
 
-          this.isLoading = false;
-        },
-        error => {
-          this.isLoading = false;
-          this.error = 'Error connecting to server.';
-        });
+            this.isLoading = false;
+          },
+          error => {
+            this.isLoading = false;
+            this.error = 'Error connecting to server.';
+          });
+      }
     }
   }
 }
