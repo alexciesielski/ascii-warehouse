09e2573edc11c6cadaa442664af457fa8ede535b
Alexander Ciesielski <alexander@ciesielski.co>
2016-06-14 10:19:39 +0200

    Removed unnecessary comments and added screenshot

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,12 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { XteamAppComponent, environment } from './app/';
 import { HTTP_PROVIDERS } from '@angular/http';
-//import { ScrollSpyService } from 'ng2-scrollspy';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrap(XteamAppComponent, [
-  HTTP_PROVIDERS,
-  //ScrollSpyService
+  HTTP_PROVIDERS
 ]);
 
